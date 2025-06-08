0a95e5bbfebfbfc64ea61168763fdedcb227db00
Jan Stusio <jan.stusio@protonmail.com>
2025-06-08 14:48:05 +0200

    feat: reimplement algorithm

--- Road-generation-alg/Road-gen-alg/Road-gen-alg/Models/Roadmap/Roadmap.swift
+++ Road-generation-alg/Road-gen-alg/Road-gen-alg/Models/Roadmap/Roadmap.swift
@@ -14,33 +14,33 @@ struct RoadNodeMetadata {
 struct RoadEdgeMetadata {
     let laneNum: Int
 }
-
-struct RoadNode: GraphNode {
-    let id: UUID
-    var metadata: RoadNodeMetadata
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    static func == (lhs: RoadNode, rhs: RoadNode) -> Bool {
-        lhs.id == rhs.id
-    }
-}
-
-struct RoadEdge: GraphEdge {
-    let id: UUID
-    let source: RoadNode
-    let destination: RoadNode
-    var metadata: RoadEdgeMetadata
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    static func == (lhs: RoadEdge, rhs: RoadEdge) -> Bool {
-        lhs.id == rhs.id
-    }
-}
-
-typealias RoadmapGraph = Graph<RoadNode, RoadEdge>
+//
+//struct RoadNode: GraphNode {
+//    let id: UUID
+//    var metadata: RoadNodeMetadata
+//    
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(id)
+//    }
+//    
+//    static func == (lhs: RoadNode, rhs: RoadNode) -> Bool {
+//        lhs.id == rhs.id
+//    }
+//}
+//
+//struct RoadEdge: GraphEdge {
+//    let id: UUID
+//    let source: RoadNode
+//    let destination: RoadNode
+//    var metadata: RoadEdgeMetadata
+//    
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(id)
+//    }
+//    
+//    static func == (lhs: RoadEdge, rhs: RoadEdge) -> Bool {
+//        lhs.id == rhs.id
+//    }
+//}
+//
+//typealias RoadmapGraph = Graph<RoadNode, RoadEdge>
