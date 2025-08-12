2b083697aba0eca42881e7d15ebf91e703728aa1
Jan Stusio <jan.stusio@protonmail.com>
2025-08-12 19:33:40 +0200

    chore: clean xcodeproj

--- gui/Road-gen-alg/ContentView.swift
+++ gui/Road-gen-alg/ContentView.swift
@@ -1,25 +1,3 @@
-//
-//  ContentView.swift
-//  Road-gen-alg
-//
-//  Created by Jan Stusio on 10/01/2025.
-//
-
-//import SwiftUI
-//
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-////            RoadNetworkView()
-//        }
-//        .padding()
-//    }
-//}
-//
-//#Preview {
-//    ContentView()
-//}
-
 import SwiftUI
 import Collections
 
